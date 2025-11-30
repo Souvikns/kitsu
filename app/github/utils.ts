38db71f0a6877d08724209561926350cd1a1efd0
Souvikns <souvikde.ns@gmail.com>
2025-11-30 20:37:24 +0530

    feat: comment pr with generated summary

--- app/github/utils.ts
+++ app/github/utils.ts
@@ -1,3 +1,4 @@
+import {Octokit} from '@octokit/rest';
 
 export async function fetchRawPatch(owner: string, repo: string, pullno: string) {
     let url = `https://patch-diff.githubusercontent.com/raw/${owner}/${repo}/pull/${pullno}.patch`
@@ -9,4 +10,23 @@ export async function fetchRawPatch(owner: string, repo: string, pullno: string)
     const text = await res.text();
 
     return text;
+}
+
+export interface makeCommentParams {
+    token: string,
+    owner: string,
+    repo: string,
+    pullNo: string,
+    summary: string
+}
+
+export async function makeComment(params: makeCommentParams) {
+    const octokit = new Octokit({auth: params.token});
+
+    await octokit.issues.createComment({
+        owner: params.owner,
+        repo: params.repo,
+        issue_number: parseInt(params.pullNo),
+        body: params.summary
+    })
 }
