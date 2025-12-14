64742ba100305e7330b8b4e8464d3fdc8e24e0d2
Souvik De <souvikde.ns@gmail.com>
2025-12-14 16:10:53 +0530

    feat: setup tests (#6)

---
+++ tests/providers/openai.test.ts
@@ -0,0 +1,23 @@
+import { OpenAIProvider } from "kitsu/providers/openai";
+
+describe("OpenAI Provider", () => {
+  it("should throw error if apikey is missing", () => {
+    try {
+      const _openai = new OpenAIProvider({
+        apikey: "",
+        model: "",
+      });
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+  });
+
+  it("should initialize with proper inputs", () => {
+    const openai = new OpenAIProvider({
+      apikey: "dummy-key",
+      model: "dummy-model",
+    });
+
+    expect(openai).toBeDefined();
+  });
+});
